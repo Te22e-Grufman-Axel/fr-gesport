1689c4bc4dc6f9e31a55e3c1a618a2d802520900
Axel Grufman <Axel.Grufman@elev.ga.ntig.se>
2023-09-08 16:15:51 +0200

    Kommentarer

--- frågesport/Program.cs
+++ frågesport/Program.cs
@@ -1,18 +1,18 @@
-﻿int points = 0;
+﻿int points = 0;  //skapar en variabel för att hålla koll på poäng
 
 Console.WriteLine("Välkommen till Axels frågesport ");
-Console.WriteLine("För att börja klicka på valfri knapp");
-Console.ReadKey();
-Console.Clear();
+Console.WriteLine("För att börja klicka på valfri knapp"); //Välkomand text
+Console.ReadKey();     //startar frågesporten
+Console.Clear();  
 
 
 // ---------------------------------------------------------------
 
 Console.WriteLine("I spelet Rust, Vilket vapen är bäst");
-Console.WriteLine("1 = AK47  2 = Lr-300   3 = Mp5");
+Console.WriteLine("1 = AK47  2 = Lr-300   3 = Mp5"); //Frågan
 
 string answer1 = Console.ReadLine();
-if (answer1 == "1")
+if (answer1 == "1") //om man får rätt
 {
 points++;
 Console.WriteLine("Rätt");
@@ -22,7 +22,7 @@ Console.Clear();
 else
 {
     Console.WriteLine("Fel");
-    Console.ReadKey();
+    Console.ReadKey(); //om man får fel
 Console.Clear();
 }
 
@@ -31,39 +31,39 @@ Console.Clear();
 
 
 Console.WriteLine("I spelet Rust, hur många helekoptrar finns det?");
-Console.WriteLine("1 = 1  2 = 3   3 = 4");
+Console.WriteLine("1 = 1  2 = 3   3 = 4");     //frågan
 
 string answer2 = Console.ReadLine();
 if (answer2 == "3")
 {
 points++;
-Console.WriteLine("Rätt");
+Console.WriteLine("Rätt"); //om man får rätt
 Console.ReadKey();
 Console.Clear();
 }
 else
 {
     Console.WriteLine("Fel");
-    Console.ReadKey();
+    Console.ReadKey(); //om man får fel
 Console.Clear();
 }
 
 // -----------------------------------------------------------------------------------
 
 Console.WriteLine(" för hur många år sen kom rust ut?");
-Console.WriteLine("1 = 5  2 = 10   3 = 15");
+Console.WriteLine("1 = 5  2 = 10   3 = 15");      //Frågan
 
 string answer3 = Console.ReadLine();
 if (answer3 == "2")
 {
 points++;
 Console.WriteLine("Rätt");
-Console.ReadKey();
+Console.ReadKey();  //om man får rätt
 Console.Clear();
 }
 else
 {
-    Console.WriteLine("Fel");
+    Console.WriteLine("Fel");  //om man får fel
     Console.ReadKey();
 Console.Clear();
 }
@@ -73,7 +73,7 @@ Console.Clear();
 
 
 
-Console.WriteLine("Villket företag äger Rust");
+Console.WriteLine("Villket företag äger Rust");     //Frågan
 Console.WriteLine("1 = Facepunch  2 = Riotgames   3 = Mojang");
 
 string answer4 = Console.ReadLine();
@@ -81,13 +81,13 @@ if (answer4 == "1")
 {
 points++;
 Console.WriteLine("Rätt");
-Console.ReadKey();
+Console.ReadKey(); //om man får rätt
 Console.Clear();
 }
 else
 {
     Console.WriteLine("Fel");
-    Console.ReadKey();
+    Console.ReadKey();    //om man får fel
 Console.Clear();
 }
 
@@ -96,13 +96,13 @@ Console.Clear();
 if (points == 0)
 {
 Console.WriteLine("DU suger");
-Console.WriteLine("Du fick ingen rätt");
+Console.WriteLine("Du fick ingen rätt");     //om man får 0 poäng
 Console.WriteLine("Du fick " + points);
 }
 else
 {
 Console.WriteLine("Du klarade det");
-Console.WriteLine("Du fick " + points + " rätt!!!");
+Console.WriteLine("Du fick " + points + " rätt!!!");  //om man får mer än 0 poäng
 
 }
 // ------------------------------
